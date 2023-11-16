3604126c1c41e3c1c6e233e4cd315059399149b6
unknown <raymondacamposandoval@gmail.com>
2023-11-16 13:54:40 -0400

    cambio de url field a imagefield

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -19,6 +19,6 @@ class Product(models.Model):
     description = models.CharField(max_length=300)
     active = models.BooleanField(default=True)
     price = models.IntegerField(default=0)
-    image = models.URLField(null=True)
+    image = models.ImageField(null=True)
     winner = models.ForeignKey(Bid, on_delete=models.CASCADE, null=True, related_name="product")
     comments = models.ForeignKey(Comment, on_delete=models.CASCADE, null=True, related_name="product")
