1228073b16c5359282d8c2712d6b69450c45162e
unknown <raymondacamposandoval@gmail.com>
2023-11-15 21:23:23 -0400

    anadiendo modelos y admin

--- commerce/auctions/admin.py
+++ commerce/auctions/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import User, Bid, Comment, Product
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Bid)
+admin.site.register(Comment)
+admin.site.register(Product)
--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,20 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Bid(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name="bids")
+    bidamount = models.IntegerField()
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name="comments")
+    comment = models.CharField(max_length=300)
+
+class Product(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name="products")
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=300)
+    active = models.BooleanField(default=True)
+    price = models.IntegerField()
+    winner = models.ForeignKey(Bid, on_delete=models.CASCADE, null=True, related_name="product")
+    comments = models.ForeignKey(Comment, on_delete=models.CASCADE, null=True, related_name="product")
